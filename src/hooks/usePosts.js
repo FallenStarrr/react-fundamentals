a7aad282371f91f7c243da9c11116b7df7dc3449
Vlad <juzopigeon@gmail.com>
2021-09-14 16:50:22 +0600

    Update usePosts.js

--- src/hooks/usePosts.js
+++ src/hooks/usePosts.js
@@ -1,3 +1,6 @@
+import React, { useState, useMemo } from "react";
+
+
 export const useSortedPosts = (posts, sort) => {
   const sortedPosts = useMemo(() => {
     console.log("ОТРАБОТАЛА ФУНКЦИЯ СОРТЕД ПОСТ");
