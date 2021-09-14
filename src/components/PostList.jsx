863654c7b43ffe17f76f80cc5324f9f5089fd072
Fallen Starrr <juzopigeon@gmail.com>
2021-09-14 16:21:53 +0600

    changes

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import PostItem from "./PostItem";
+import { TransitionGroup, CSSTransition } from "react-transition-group";
 
 const PostList = ({ posts, title, remove }) => {
   if (!posts.length) {
@@ -9,14 +10,13 @@ const PostList = ({ posts, title, remove }) => {
   return (
     <div>
       <h1>{title}</h1>
-      {posts.map((post, index) => (
-        <PostItem
-          remove={remove}
-          number={index + 1}
-          post={post}
-          key={post.id}
-        />
-      ))}
+      <TransitionGroup>
+        {posts.map((post, index) => (
+          <CSSTransition key={post.id} timeout={500} classNames="post">
+            <PostItem remove={remove} number={index + 1} post={post} />
+          </CSSTransition>
+        ))}
+      </TransitionGroup>
     </div>
   );
 };
