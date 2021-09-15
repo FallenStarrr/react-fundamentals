a325de99291d7d514d64c010e282ca3eefa28ae2
Fallen Starrr <juzopigeon@gmail.com>
2021-09-15 09:15:46 +0600

    animations

--- src/App.jsx
+++ src/App.jsx
@@ -9,31 +9,14 @@ import "./styles/App.css";
 
 
 function App() {
-  const [posts, setPosts] = useState([
-    { id: 1, title: "Javascript", body: "бб" },
-    { id: 2, title: "Javascript 2", body: "аа" },
-    { id: 3, title: "Javascript 3", body: "яя" },
-  ]);
+  const [posts, setPosts] = useState([]);
 
   const [filter, setFilter] = useState({ sort: "", query: "" });
   const [modal, setModal] = useState(false);
+  const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query)
+  
 
-  const sortedPosts = useMemo(() => {
-    console.log("ОТРАБОТАЛА ФУНКЦИЯ СОРТЕД ПОСТ");
-    if (filter.sort) {
-      return [...posts].sort((a, b) =>
-        a[filter.sort].localeCompare(b[filter.sort])
-      );
-    }
-    return posts;
-  }, [filter.sort, posts]);
-
-  const sortedAndSearchedPosts = useMemo(() => {
-    return sortedPosts.filter((post) =>
-      post.title.toLowerCase().includes(filter.query.toLowerCase())
-    );
-  }, [filter.query, sortedPosts]);
-
+  
   const createPost = (newPost) => {
     setPosts([...posts, newPost]);
     setModal(false);
