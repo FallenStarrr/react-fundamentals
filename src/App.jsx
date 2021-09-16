e095f01d4036259b352489da03ac51259fc7de26
Fallen Starrr <juzopigeon@gmail.com>
2021-09-16 14:26:53 +0600

    ch

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,15 @@ import About from "./pages/About";
 import "./styles/App.css";
 import Posts from "./pages/Posts";
 import Navbar from "./components/UI/navbar/Navbar";
+import AppRouter from "./components/AppRouter";
+
 import Error from "./pages/Error";
 
 function App() {
   return (
     <BrowserRouter>
       <Navbar />
-      
+      <AppRouter/>
     </BrowserRouter>
   );
 }
