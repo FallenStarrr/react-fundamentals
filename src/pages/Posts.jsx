1366fcdea2e008d6fc24e17a305bda2d8bfc118a
Fallen Starrr <juzopigeon@gmail.com>
2021-09-16 16:51:02 +0600

    limit control

--- src/pages/Posts.jsx
+++ src/pages/Posts.jsx
@@ -11,6 +11,7 @@ import { useFetching } from "../hooks/useFetching";
 import { getPageCount } from "../utils/pages";
 import Pagination from "../components/UI/pagination/Pagination";
 import { useObserver } from "../hooks/useObserver";
+import MySelect from "../components/UI/select/MySelect";
 
 function Posts() {
   const [posts, setPosts] = useState([]);
@@ -39,7 +40,7 @@ function Posts() {
 
   useEffect(() => {
     fetchPosts(limit, page);
-  }, [page]);
+  }, [page , limit]);
 
   const createPost = (newPost) => {
     setPosts([...posts, newPost]);
@@ -66,6 +67,17 @@ function Posts() {
 
       <hr style={{ margin: "15px 0" }} />
       <PostFilter filter={filter} setFilter={setFilter} />
+      <MySelect 
+        value={limit}
+        onChange={value => setLimit(value)}
+        defaultValue="Кол-во элементов на странице"
+        options={[
+          {value: 5, name: '5'},
+          {value: 10, name: '10'},
+          {value: 25, name: '25'},
+          {value: -1, name: 'Показать все'}
+        ]}
+      />
       {postError && <h1>Произошка ошибка </h1>}
       <PostList
           remove={removePost}
